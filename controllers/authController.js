826d4f340301d828e13d5f6ddee69c99c598df6f
unknown <naeemghadai007.com@gmail.com>
2022-12-31 23:38:44 +0530

    sending roles in the login response and added credentials: true in corsOptions

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,7 +15,7 @@ const handleLogin = async (req, res) => {
   //evaluate password
   const match = await bcrypt.compare(pwd, foundUser.password);
   if (match) {
-    const roles = Object.values(foundUser.roles);
+    const roles = Object.values(foundUser.roles).filter(Boolean);
     //create JWT
     const accessToken = jwt.sign(
       {
@@ -35,11 +35,11 @@ const handleLogin = async (req, res) => {
     console.log(result);
     res.cookie("jwt", refreshToken, {
       httpOnly: true,
-      // sameSite: "none",
-      // secure: true,
+      sameSite: "none",
+      secure: true,
       maxAge: 24 * 60 * 60 * 1000,
     });
-    res.json({ accessToken });
+    res.json({ roles, accessToken });
   } else {
     res.sendStatus(401);
   }
